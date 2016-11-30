decde4a54bb2c81cf059d4744407e3823e8b7f2a
Daniel <danimaxi60@hotmail.com>
2016-11-30 12:23:37 -0500

    creacion de la clase persona

---
+++ Proyecto/Proyecto/Persona.vb
@@ -0,0 +1,63 @@
+﻿Public Class Persona
+
+    Private _id As Integer
+    Public Property ID() As Integer
+        Get
+            Return _id
+        End Get
+        Set(ByVal value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Private _cedula As String
+    Public Property Cedula() As String
+        Get
+            Return _cedula
+        End Get
+        Set(ByVal value As String)
+            _cedula = value
+        End Set
+    End Property
+
+    Private _nombre As String
+    Public Property Nombre() As String
+        Get
+            Return _nombre
+        End Get
+        Set(ByVal value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Private _apellido As String
+    Public Property Apellido() As String
+        Get
+            Return _apellido
+        End Get
+        Set(ByVal value As String)
+            _apellido = value
+        End Set
+    End Property
+
+    Private _edad As Char
+    Public Property Edad() As Char
+        Get
+            Return _edad
+        End Get
+        Set(ByVal value As Char)
+            _edad = value
+        End Set
+    End Property
+
+    Private _nacionalidad As String
+    Public Property Nacionalidad() As String
+        Get
+            Return _nacionalidad
+        End Get
+        Set(ByVal value As String)
+            _nacionalidad = value
+        End Set
+    End Property
+
+End Class
