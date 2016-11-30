76bb51485108232743d01ceeb1317aca231d309a
Daniel <danimaxi60@hotmail.com>
2016-11-30 13:38:37 -0500

    clase administrador

---
+++ Proyecto/Proyecto/Administrador.vb
@@ -0,0 +1,44 @@
+﻿Public Class Administrador
+
+
+    Private _usuario As String
+    Public Property Usuario() As String
+        Get
+            Return _Usuario
+        End Get
+        Set(ByVal value As String)
+            _Usuario = value
+        End Set
+    End Property
+
+    Private _contraseña As String
+    Public Property Contraseña() As String
+        Get
+            Return _Contraseña
+        End Get
+        Set(ByVal value As String)
+            _Contraseña = value
+        End Set
+    End Property
+
+    Private _cargo As String
+    Public Property Cargo() As String
+        Get
+            Return _Cargo
+        End Get
+        Set(ByVal value As String)
+            _Cargo = value
+        End Set
+    End Property
+
+
+
+    Public Sub New()
+
+    End Sub
+
+    Private Sub RealizarReporte()
+
+    End Sub
+
+End Class
